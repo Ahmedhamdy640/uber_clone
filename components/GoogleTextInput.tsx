15d3ba9da05b67f1f3002d623b7a2971d4ec3337
Ahmedhamdy640 <ah812185@gmial.com>
2024-09-24 15:40:45 +0300

    add map features

--- components/GoogleTextInput.tsx
+++ components/GoogleTextInput.tsx
@@ -1,6 +1,10 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 import { GoogleInputProps } from "@/types/type";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { icons } from "@/constants";
+
+const googlePlacesApiKey = process.env.EXPO_PUBLIC_GOOGLE_API_KEY;
 
 const GoogleTextInput = ({
   icon,
@@ -13,7 +17,66 @@ const GoogleTextInput = ({
     <View
       className={`flex flex-row items-center justify-center relative z-50 rounded-xl mb-5 ${containerStyle}`}
     >
-      <Text>search</Text>
+      <GooglePlacesAutocomplete
+        fetchDetails={true}
+        placeholder="Where you want to go?"
+        debounce={200}
+        styles={{
+          textInputContainer: {
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 20,
+            marginHorizontal: 20,
+            position: "relative",
+            shadowColor: "#d4d4d4",
+          },
+          textInput: {
+            backgroundColor: textInputBackgroundColor || "white",
+            fontSize: 16,
+            fontWeight: "600",
+            marginTop: 5,
+            width: "100%",
+            borderRadius: 200,
+          },
+          listView: {
+            backgroundColor: textInputBackgroundColor || "white",
+            position: "relative",
+            top: 0,
+            width: "100%",
+            borderRadius: 10,
+            shadowColor: "#d4d4d4",
+            zIndex: 99,
+          },
+        }}
+        onPress={(data, details = null) => {
+          handlePress({
+            latitude: details?.geometry.location.lat,
+            longitude: details?.geometry.location.lng,
+            address: data.description,
+          });
+        }}
+        query={{
+          key: googlePlacesApiKey,
+          language: "ar",
+          components: "country:eg",
+          types: "establishment",
+          radius: 20000,
+          location: initialLocation,
+        }}
+        renderLeftButton={() => (
+          <View className="justify-center items-center w-6 h-6">
+            <Image
+              source={icon ? icon : icons.search}
+              className="w-6 h-6"
+              resizeMode="contain"
+            />
+          </View>
+        )}
+        textInputProps={{
+          placeholderTextColor: "gray",
+          placeholder: initialLocation ?? "Where you want to go?",
+        }}
+      />
     </View>
   );
 };
---
+++ components/RideLayout.tsx
@@ -0,0 +1,53 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { router } from "expo-router";
+import { icons } from "@/constants";
+import Map from "./Map";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+
+const RideLayout = ({
+  title,
+  children,
+  snapPoints,
+}: {
+  title: string;
+  children: React.ReactNode;
+  snapPoints?: string[];
+}) => {
+  const bottomSheetRef = React.useRef<BottomSheet>(null);
+  return (
+    <GestureHandlerRootView>
+      <View className="flex-1 bg-white">
+        <View className="flex flex-col h-screen bg-blue-500">
+          <View className="flex flex-row absolute z-10 top-16 items-center justify-start px-5">
+            <TouchableOpacity onPress={() => router.back()}>
+              <View className="w-10 h-10 bg-white rounded-full items-center justify-center">
+                <Image
+                  source={icons.backArrow}
+                  className="w-6 h-6"
+                  resizeMode="contain"
+                />
+              </View>
+            </TouchableOpacity>
+            <Text className="text-xl font-JakartaBold ml-5">
+              {title || "Go Back"}
+            </Text>
+          </View>
+          <Map />
+        </View>
+        <BottomSheet
+          ref={bottomSheetRef}
+          snapPoints={snapPoints || ["40%", "85%"]}
+          index={0}
+        >
+          <BottomSheetView style={{ flex: 1, padding: 20 }}>
+            {children}
+          </BottomSheetView>
+        </BottomSheet>
+      </View>
+    </GestureHandlerRootView>
+  );
+};
+
+export default RideLayout;
